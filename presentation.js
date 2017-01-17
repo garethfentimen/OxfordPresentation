b8cb7eae594e5e776bceee63fccb0fd9285e38b1
Gareth Fentimen <gareth.fentimen@it.ox.ac.uk>
2017-01-17 12:40:39 +0000

    More content and animation

--- presentation.js
+++ presentation.js
@@ -1,3 +1,47 @@
 (function(reveal) {
     reveal.initialize();
+
+    var slideRightClassName = "slideright",
+        showClass = "show",
+        fragment = null;
+
+
+    reveal.addEventListener('fragmentshown', function( event ) {
+        this.fragment = event.fragment;
+        
+        if (!this.fragment.classList)
+        {
+            return false;
+        }
+
+        var arrayOfClasses = [];
+        this.fragment.classList.forEach(function(value, key, listObj, argument) {
+            arrayOfClasses.push(value);
+        });
+
+        if (arrayOfClasses.includes(slideRightClassName) && !arrayOfClasses.includes(showClass))
+        {
+            this.fragment.classList.add(showClass);
+        }
+    });
+
+    reveal.addEventListener('fragmenthidden', function( event ) {
+        this.fragment = event.fragment;
+        if (!this.fragment.classList)
+        {
+            return false;
+        }
+
+        var arrayOfClasses = [];
+        this.fragment.classList.forEach(function(value, key, listObj, argument) {
+            arrayOfClasses.push(value);
+        });
+
+        if (arrayOfClasses.includes(slideRightClassName) && arrayOfClasses.includes(showClass)) {
+            this.fragment.classList.remove(showClass);
+        }
+    });
 })(Reveal);
+
+
+// polyfills
